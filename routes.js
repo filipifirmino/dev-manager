c031047fdbe2150af01c100acbd298c13d7f163e
Filipi Firmino <contatoic3web@gmail.com>
2021-06-22 12:20:16 -0300

    Criando as rotas ainda sem respostas

--- routes.js
+++ routes.js
@@ -6,4 +6,20 @@ routes.get('/', (_req, res) => {
     res.json('Welcome to Dev Manager!');
 });
 
+routes.get('/developer', (_req, res) => {
+    res.json('Get All Developers');
+})
+routes.post('/developer', (_req, res) => {
+    res.json('Create Developer');
+})
+
+routes.put('/developer/:id', (_req, res) => {
+    res.json('Update Developer');
+})
+
+routes.delete('/developer/:id', (_req, res) => {
+    res.json('Delete developer');
+})
+
+
 module.exports = routes
