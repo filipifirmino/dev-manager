e377ff3e9331e54eac6ec876a31d9342b2bbae41
Filipi Firmino <contatoic3web@gmail.com>
2021-06-22 20:58:42 -0300

    Criando models

---
+++ src/models/developer.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Developer extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Developer.init({
+    name: DataTypes.STRING,
+    fone: DataTypes.STRING,
+    celular: DataTypes.STRING,
+    endereco: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Developer',
+  });
+  return Developer;
+};
---
+++ src/models/developertechnologies.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class DeveloperTechnologies extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.Developer.belongsToMany(models.Technologies, {
+        as: 'technologies',
+        through: Developer,
+        foreignKey: 'id',
+        otherKey: 'id',
+      });
+      models.Technologies.belongsToMany(models.Developer, {
+        as:'developer',
+        through: Technologies,
+        foreignKey: 'id',
+        otherKey: 'id',
+      })
+    }
+  };
+  DeveloperTechnologies.init({
+    devId: DataTypes.INTEGER,
+    tecId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'DeveloperTechnologies',
+    timestamps: false,
+  });
+  return DeveloperTechnologies;
+};
