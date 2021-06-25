0ae460daf02146288cf3cfe77f84b6e74b04dbf1
Filipi Firmino <contatoic3web@gmail.com>
2021-06-25 18:17:37 -0300

    Implementado a deleçao de relacionamento

--- src/controllers/deleteDeveloper.js
+++ src/controllers/deleteDeveloper.js
@@ -1,10 +1,11 @@
-const { Developer } = require("../models");
+const { Developer, DeveloperTechnologies } = require("../models");
 
 const deleteDeveloper = async (req, res) => {
   const { id } = req.params;
 
   try {
     Developer.destroy({ where: { id: id } });
+    DeveloperTechnologies.destroy({where: {devId: id}});
     res.status(200).json({ message: "successfully deleted" });
   } catch (error) {
     res.status(403).json({ message: "Failed to delete developer" });
