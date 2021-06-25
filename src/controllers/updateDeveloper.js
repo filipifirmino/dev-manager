6a1cc0304508a8e51695d8d947dc14be0a83e813
Filipi Firmino <contatoic3web@gmail.com>
2021-06-25 17:46:59 -0300

    adcionando async no controller

--- src/controllers/updateDeveloper.js
+++ src/controllers/updateDeveloper.js
@@ -1,13 +1,16 @@
 const { Developers, DeveloperTechnologies } = require("../models"); //Trigger
 
-const updateDeveloper = (req, res) => {
+const updateDeveloper = async (req, res) => {
   const { nome, fone, celular, endereco, tecnologias } = req.body;
   const { id } = req.params;
 
-  Developers.update({ nome, fone, celular, endereco }, { where: { id: id } });
-  tecnologias.forEach((actual) => {
-    DeveloperTechnologies.update({ tecId: actual }, { where: { devId: id } });
-  });
+  try {
+    Developers.update({ nome, fone, celular, endereco }, { where: { id: id } });
+    tecnologias.forEach((actual) => {
+      DeveloperTechnologies.update({ tecId: actual }, { where: { devId: id } });
+    });
+    return res.status(200).json({message: "successfully!"})
+  } catch (error) { res.status(403).json({message: "Failure delete"})}
 };
 
 module.exports = {
